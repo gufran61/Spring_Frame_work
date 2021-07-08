83e03b0d3044d9281d87080951e6cd97c52c7ea5
gufran61 <you@example.com>
2021-07-08 15:59:30 +0530

    16.Implementing Bean LifeCyle using Annotations - @PostConstruct - @PreDestroy -

--- src/main/java/com/springcore/lifecycle/Main.java
+++ src/main/java/com/springcore/lifecycle/Main.java
@@ -5,14 +5,16 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-public static void main(String[] args) {
-	AbstractApplicationContext ac = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
-	ac.registerShutdownHook();
-	/*
-	 * Samosa bean = (Samosa) ac.getBean("s"); System.out.println(bean.getPrice());
-	 */
-	
-	Pepsi pep = (Pepsi) ac.getBean("p");
-	System.out.println(pep);
-}
+	public static void main(String[] args) {
+		AbstractApplicationContext ac = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
+		ac.registerShutdownHook();
+		/*
+		 * Samosa bean = (Samosa) ac.getBean("s"); System.out.println(bean.getPrice());
+		 * 
+		 * 
+		 * Pepsi pep = (Pepsi) ac.getBean("p"); System.out.println(pep);
+		 */
+		Example bean = (Example) ac.getBean("example");
+		System.out.println(bean);
+	}
 }
