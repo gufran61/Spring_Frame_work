aca7c63157f77c829ff6cd8a5f95dc98c4638441
gufran61 <you@example.com>
2021-07-16 18:20:34 +0530

    21.Spring Standalone Collections[List,Map,Properties] - Util Schema in Spring

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -15,8 +15,14 @@ public class App
     {
     	
     	ApplicationContext ac=new ClassPathXmlApplicationContext("com/springcore/config.xml");
-    	Addition bean = (Addition) ac.getBean("add");
+    	Emp bean = (Emp) ac.getBean("emp");
     	System.out.println(bean);
-    	bean.doSum();
+    	System.out.println(bean.getFriends().getClass().getName());
+    	System.out.println("---------------------------------------");
+    	System.out.println(bean.getFeesStructure());
+    	System.out.println(bean.getFeesStructure().getClass().getName());
+    	System.out.println("-------------------------------");
+    	System.out.println(bean.getProp());
+    	System.out.println(bean.getProp().getClass().getName());
     }
 }
--- src/main/java/com/springcore/Emp.java
+++ src/main/java/com/springcore/Emp.java
@@ -2,51 +2,41 @@ package com.springcore;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Properties;
 
 public class Emp {
-private String name;
-private List<String>phones;
-private Set<String> adress;
-private Map<String, String> course;
-public Emp() {
-	super();
-	// TODO Auto-generated constructor stub
-}
-public Emp(String name, List<String> phones, Set<String> adress, Map<String, String> course) {
-	super();
-	this.name = name;
-	this.phones = phones;
-	this.adress = adress;
-	this.course = course;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public List<String> getPhones() {
-	return phones;
-}
-public void setPhones(List<String> phones) {
-	this.phones = phones;
-}
-public Set<String> getAdress() {
-	return adress;
-}
-public void setAdress(Set<String> adress) {
-	this.adress = adress;
-}
-public Map<String, String> getCourse() {
-	return course;
-}
-public void setCourse(Map<String, String> course) {
-	this.course = course;
-}
+private List<String>friends;
+private Map<String, Integer>feesStructure;
+private Properties prop;
+
+
 @Override
 public String toString() {
-	return "Emp [name=" + name + ", phones=" + phones + ", adress=" + adress + ", course=" + course + "]";
+	return "Emp [friends=" + friends + ", feesStructure=" + feesStructure + ", prop=" + prop + "]";
+}
+
+public Properties getProp() {
+	return prop;
+}
+
+public void setProp(Properties prop) {
+	this.prop = prop;
+}
+
+public Map<String, Integer> getFeesStructure() {
+	return feesStructure;
+}
+
+public void setFeesStructure(Map<String, Integer> feesStructure) {
+	this.feesStructure = feesStructure;
+}
+
+public List<String> getFriends() {
+	return friends;
+}
+
+public void setFriends(List<String> friends) {
+	this.friends = friends;
 }
 
 }
